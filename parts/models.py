399b7fdd0c3333fd49d5ed64819b2d147be6fce6
rustem <200103424@stu.sdu.edu.kz>
2023-10-29 15:17:10 +0600

    initial commit

--- parts/models.py
+++ parts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Details(models.Model):
+    name = models.CharField(max_length=100)
+    schema_name = models.CharField(max_length=100)
+    schema_number = models.CharField(max_length=100)
+    drawing_number = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    parent_components = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+    def __str__(self):
+        return self.name
--- parts/views.py
+++ parts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def get_component_tree(request):
+    components = Details.objects.all()
+
+    def build_tree(node):
+        children = Details.objects.filter(parent_components__in=[node])
+        return {'component': node, 'children': [build_tree(child) for child in children]}
+
+    # Строим дерево начиная с корневых компонент
+    root_components = Details.objects.filter(parent_components=None)
+    tree = [build_tree(root) for root in root_components]
+
+    return render(request, 'parts/component-tree.html', {'tree': tree})
